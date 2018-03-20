fe49add784d2292ab0de36f1f50390827e49f4a7
Riley Trautman <rileyt@spiceworks.com>
2018-03-20 00:32:52 -0500

    Clean event actor

--- src/actors/timer/mod.rs
+++ src/actors/timer/mod.rs
@@ -21,7 +21,7 @@
 //!
 //! It handles notifying telegram when events are soon, starting, and ending.
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use actix::{Addr, Arbiter, Syn, Unsync};
 use chrono::{DateTime, Duration as OldDuration, Timelike};
@@ -40,6 +40,7 @@ use util::flatten;
 mod actor;
 pub mod messages;
 
+#[derive(Clone, Debug, Hash)]
 enum TimerState {
     WaitingNotify,
     WaitingStart,
@@ -50,12 +51,7 @@ enum TimerState {
 pub struct Timer {
     db: Addr<Unsync, DbBroker>,
     tg: Addr<Syn, TelegramActor>,
-    waiting_notify: Vec<HashSet<i32>>,
-    waiting_start: Vec<HashSet<i32>>,
-    waiting_end: Vec<HashSet<i32>>,
-    futures: Vec<HashSet<i32>>,
-    states: HashMap<i32, TimerState>,
-    events: HashMap<i32, Event>,
+    times: Vec<HashMap<i32, (TimerState, Event)>>,
 }
 
 impl Timer {
@@ -63,113 +59,75 @@ impl Timer {
         Timer {
             db,
             tg,
-            waiting_notify: (0..60).map(|_| HashSet::new()).collect(),
-            waiting_start: (0..60).map(|_| HashSet::new()).collect(),
-            waiting_end: (0..60).map(|_| HashSet::new()).collect(),
-            futures: (0..60).map(|_| HashSet::new()).collect(),
-            states: HashMap::new(),
-            events: HashMap::new(),
+            times: (0..60).map(|_| HashMap::new()).collect(),
         }
     }
 
     /// Notify telegram of any events starting in the next 45 minutes, if a notification has not
     /// already been sent
-    fn migrate_notifies(&mut self, now: DateTime<Utc>) {
-        let notify_time = now + OldDuration::minutes(45);
-        let index = notify_time.minute() as usize;
+    fn migrate_notify(&mut self, index: usize, event: Event) {
+        debug!("Moving event {} to waiting_start", event.id());
 
-        let mut ids = self.waiting_notify[index].clone();
-        self.waiting_notify[index] = HashSet::new();
-
-        for event_id in ids.drain() {
-            if let Some(event) = self.events.get(&event_id).cloned() {
-                debug!("Moving event {} to waiting_start", event_id);
-                self.notify_soon(event);
-                self.states.insert(event_id, TimerState::WaitingStart);
-                self.waiting_start[index].insert(event_id);
-            } else {
-                error!("Event {} is missing", event_id);
-                self.waiting_notify[index].remove(&event_id);
-                self.states.remove(&event_id);
-            }
-        }
+        self.notify_soon(event.clone());
+        self.times[index].insert(event.id(), (TimerState::WaitingStart, event));
     }
 
     /// Notify telegram of any events that have started, if a notification has not already been sent
-    fn migrate_starts(&mut self, now: DateTime<Utc>) {
-        let index = now.minute() as usize;
+    fn migrate_start(&mut self, next_hour: DateTime<Utc>, index: usize, event: Event) {
+        let end_index = event.end_date().minute() as usize;
+        self.times[index].remove(&event.id());
 
-        let mut ids = self.waiting_start[index].clone();
-        self.waiting_start[index] = HashSet::new();
-
-        let hour_from_now = now + OldDuration::hours(1);
-
-        for event_id in ids.drain() {
-            if let Some(event) = self.events.get(&event_id).cloned() {
-                let end_index = event.end_date().minute() as usize;
-
-                if hour_from_now > event.end_date().with_timezone(&Utc) {
-                    debug!("Moving event {} to waiting_end", event_id);
-                    self.states.insert(event_id, TimerState::WaitingEnd);
-                    self.waiting_end[end_index].insert(event_id);
-                } else {
-                    debug!("Moving event {} to futures", event_id);
-                    self.states.insert(event_id, TimerState::Future);
-                    self.futures[end_index].insert(event_id);
-                }
-                self.notify_now(event);
-            } else {
-                error!("Event {} is missing", event_id);
-                self.waiting_start[index].remove(&event_id);
-                self.states.remove(&event_id);
-            }
+        if next_hour > event.end_date().with_timezone(&Utc) {
+            debug!("Moving event {} to waiting_end", event.id());
+            self.times[end_index].insert(event.id(), (TimerState::WaitingEnd, event.clone()));
+        } else {
+            debug!("Moving event {} to futures", event.id());
+            self.times[end_index].insert(event.id(), (TimerState::Future, event.clone()));
         }
+
+        self.notify_now(event);
     }
 
     /// Store events that are happening now, but aren't ending for a while.
-    fn migrate_futures(&mut self, now: DateTime<Utc>) {
-        let next_hour = now + OldDuration::hours(1);
-        let index = now.minute() as usize;
-
-        for event_id in self.futures[index].clone() {
-            if let Some(event) = self.events.get(&event_id).cloned() {
-                if next_hour > event.end_date().with_timezone(&Utc) {
-                    debug!("Moving event {} to waiting_end", event_id);
-                    self.futures[index].remove(&event_id);
-                    self.states.insert(event_id, TimerState::WaitingEnd);
-                    self.waiting_end[index].insert(event_id);
-                }
-            } else {
-                error!("Event {} is missing", event_id);
-                self.futures[index].remove(&event_id);
-                self.states.remove(&event_id);
-            }
+    fn migrate_future(&mut self, next_hour: DateTime<Utc>, index: usize, event: Event) {
+        if next_hour > event.end_date().with_timezone(&Utc) {
+            debug!("Moving event {} to waiting_end", event.id());
+            self.times[index].insert(event.id(), (TimerState::WaitingEnd, event));
         }
     }
 
     /// Notify telegram when an event has ended, if it has not already done so
-    fn migrate_ends(&mut self, now: DateTime<Utc>) {
-        let index = now.minute() as usize;
-
-        let mut ids = self.waiting_end[index].clone();
-        self.waiting_end[index] = HashSet::new();
-
-        for event_id in ids.drain() {
-            if let Some(event) = self.events.remove(&event_id) {
-                self.states.remove(&event_id);
-                self.delete_event(event);
-            }
-        }
+    fn migrate_end(&mut self, index: usize, event: Event) {
+        debug!("Removing completed event {}", event.id());
+        self.times[index].remove(&event.id());
+        self.delete_event(event);
     }
 
     fn migrate_events(&mut self) {
         debug!("Migrating events");
         let now = Utc::now();
+        let next_hour = now + OldDuration::hours(1);
 
-        self.migrate_futures(now);
-        self.migrate_notifies(now);
-        self.migrate_starts(now);
-        self.migrate_ends(now);
+        let index = now.minute() as usize;
+
+        for (event_id, (state, event)) in self.times[index].clone() {
+            debug!("Checking event {}", event_id);
+
+            match state {
+                TimerState::WaitingNotify => {
+                    self.migrate_notify(index, event);
+                }
+                TimerState::WaitingStart => {
+                    self.migrate_start(next_hour, index, event);
+                }
+                TimerState::WaitingEnd => {
+                    self.migrate_end(index, event);
+                }
+                TimerState::Future => {
+                    self.migrate_future(next_hour, index, event);
+                }
+            }
+        }
     }
 
     fn get_next_hour(&self) -> impl Future<Item = Vec<Event>, Error = EventError> {
@@ -191,36 +149,22 @@ impl Timer {
         }
     }
 
+    /// Search all stored events for event with ID `event_id`
+    fn remove_event(&mut self, event_id: i32) -> Option<(TimerState, Event)> {
+        self.times
+            .iter_mut()
+            .find(|map| map.contains_key(&event_id))
+            .and_then(|map| map.remove(&event_id))
+    }
+
+    /// Check if we're tracking the event with ID `event_id`
+    fn tracking_event(&self, event_id: i32) -> bool {
+        self.times.iter().any(|map| map.contains_key(&event_id))
+    }
+
     /// Properly place and notify telegram of an updated event
     fn update_event(&mut self, event: Event) {
-        if let Some(state) = self.states.remove(&event.id()) {
-            if let Some(old_event) = self.events.remove(&event.id()) {
-                match state {
-                    TimerState::WaitingNotify => {
-                        let notify_time =
-                            old_event.start_date().to_owned() - OldDuration::minutes(45);
-                        let notify_index = notify_time.minute() as usize;
-
-                        self.waiting_notify[notify_index].remove(&event.id());
-                    }
-                    TimerState::WaitingStart => {
-                        let start_index = old_event.start_date().minute() as usize;
-
-                        self.waiting_start[start_index].remove(&event.id());
-                    }
-                    TimerState::WaitingEnd => {
-                        let end_index = old_event.end_date().minute() as usize;
-
-                        self.waiting_end[end_index].remove(&event.id());
-                    }
-                    TimerState::Future => {
-                        let end_index = old_event.end_date().minute() as usize;
-
-                        self.futures[end_index].remove(&event.id());
-                    }
-                }
-            }
-        }
+        self.remove_event(event.id());
 
         self.new_event(event, Utc::now());
     }
@@ -229,55 +173,46 @@ impl Timer {
     fn new_event(&mut self, event: Event, now: DateTime<Utc>) {
         debug!("Handling event");
 
-        match self.states.get(&event.id()) {
-            Some(&TimerState::WaitingNotify) => (),
-            Some(&TimerState::WaitingStart) => (),
-            Some(&TimerState::WaitingEnd) => (),
-            Some(&TimerState::Future) => (),
-            None => {
-                debug!("New event!");
-                let start = event.start_date().with_timezone(&Utc);
-                let end = event.end_date().with_timezone(&Utc);
+        if !self.tracking_event(event.id()) {
+            debug!("New event!");
+            let start = event.start_date().with_timezone(&Utc);
+            let end = event.end_date().with_timezone(&Utc);
 
-                let should_have_ended = now > end;
-                let ending_soon = now + OldDuration::hours(1) > end;
-                let should_have_started = now > start;
-                let starting_soon = now + OldDuration::minutes(45) > start;
-                let should_drop = now + OldDuration::hours(1) < start;
+            let should_have_ended = now > end;
+            let ending_soon = now + OldDuration::hours(1) > end;
+            let should_have_started = now > start;
+            let starting_soon = now + OldDuration::minutes(45) > start;
+            let should_drop = now + OldDuration::hours(1) < start;
 
-                if should_have_ended {
-                    debug!("Should have ended");
-                    // delete event
-                    self.delete_event(event);
-                } else {
-                    if should_have_started {
-                        debug!("Should have started");
-                        // notify start
-                        self.notify_now(event.clone());
+            if should_have_ended {
+                debug!("Should have ended");
+                // delete event
+                self.delete_event(event);
+            } else {
+                if should_have_started {
+                    debug!("Should have started");
+                    // notify start
+                    self.notify_now(event.clone());
 
-                        let end_index = event.end_date().minute() as usize;
+                    let end_index = event.end_date().minute() as usize;
 
-                        if ending_soon {
-                            debug!("Ending soon");
-                            self.waiting_end[end_index].insert(event.id());
-                            self.states.insert(event.id(), TimerState::WaitingEnd);
-                        } else {
-                            debug!("Not ending soon");
-                            self.futures[end_index].insert(event.id());
-                            self.states.insert(event.id(), TimerState::Future);
-                        }
-                    } else if starting_soon {
-                        debug!("Starting soon");
-                        self.waiting_start[event.start_date().minute() as usize].insert(event.id());
-                        self.notify_soon(event.clone());
-                    } else if !should_drop {
-                        debug!("Waiting");
-                        self.waiting_notify[event.start_date().minute() as usize]
-                            .insert(event.id());
-                        self.states.insert(event.id(), TimerState::WaitingNotify);
+                    if ending_soon {
+                        debug!("Ending soon");
+                        self.times[end_index].insert(event.id(), (TimerState::WaitingEnd, event));
+                    } else {
+                        debug!("Not ending soon");
+                        self.times[end_index].insert(event.id(), (TimerState::Future, event));
                     }
+                } else if starting_soon {
+                    debug!("Starting soon");
+                    self.notify_soon(event.clone());
 
-                    self.events.insert(event.id(), event);
+                    self.times[event.start_date().minute() as usize]
+                        .insert(event.id(), (TimerState::WaitingStart, event));
+                } else if !should_drop {
+                    debug!("Waiting");
+                    self.times[event.start_date().minute() as usize]
+                        .insert(event.id(), (TimerState::WaitingNotify, event));
                 }
             }
         }
